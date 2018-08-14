2631fad73e06e0e2a59b590188e3191281ff949a
Thorsten Zoerner <me@thorsten-zoerner.com>
2018-08-14 17:47:15 +0200

    Added Support for Start-Stop Daemon Allow install as global bin

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
-require('dotenv').config();
-var service = require("corrently-node");
-service(function(db,kv) {
-  var discovergy = require("./discovergy.js");
-  var meters=process.env.DISCOVERGY_METERS.split(",");
+#!/usr/bin/env node
 
-  var updateMeter=function() {
-      if(meters.length>0) {
-          var meterid = meters.pop();
-          discovergy(db,meterid,function() {
-              updateMeter();
-          });
-      } else {
-          console.log("Start seeding");
-          // process.exit();
-      }
-  }
-  updateMeter();
-  setInterval(updateMeter,process.env.IDLE_REPUBLISH);
+var startStopDaemon = require('start-stop-daemon');
+
+startStopDaemon({}, function() {
+
+  require('dotenv').config();
+  var service = require("corrently-node");
+  service(function(db,kv) {
+    var discovergy = require("./discovergy.js");
+    var meters=process.env.DISCOVERGY_METERS.split(",");
+
+    var updateMeter=function() {
+        if(meters.length>0) {
+            var meterid = meters.pop();
+            discovergy(db,meterid,function() {
+                updateMeter();
+            });
+        } else {
+            console.log("Start seeding");
+            // process.exit();
+        }
+    }
+    updateMeter();
+    setInterval(updateMeter,process.env.IDLE_REPUBLISH);
+  });
 });
