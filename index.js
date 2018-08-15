8d70e420214fbf1dd3f9eecc981b1877ef9fdecd
Thorsten Zoerner <me@thorsten-zoerner.com>
2018-08-15 13:06:59 +0200

    Use Swarm Config if available

--- index.js
+++ index.js
@@ -5,23 +5,74 @@ var startStopDaemon = require('start-stop-daemon');
 startStopDaemon({}, function() {
 
   require('dotenv').config();
+  var crypto = require('crypto');
+  var encrypt=function(text,password){
+    var cipher = crypto.createCipher('aes-256-ctr',password)
+    var crypted = cipher.update(text,'utf8','hex')
+    crypted += cipher.final('hex');
+    return crypted;
+  }
+
+  var decrypt=function(text,password){
+    var decipher = crypto.createDecipher('aes-256-ctr',password)
+    var dec = decipher.update(text,'hex','utf8')
+    dec += decipher.final('utf8');
+    return dec;
+  }
+
   var service = require("corrently-node");
   service(function(db,kv) {
-    var discovergy = require("./discovergy.js");
-    var meters=process.env.DISCOVERGY_METERS.split(",");
+    var discovergyService=function() {
 
-    var updateMeter=function() {
-        if(meters.length>0) {
-            var meterid = meters.pop();
-            discovergy(db,meterid,function() {
-                updateMeter();
-            });
-        } else {
-            console.log("Start seeding");
-            // process.exit();
-        }
+      var discovergy = require("./discovergy.js");
+      var meters=process.env.DISCOVERGY_METERS.split(",");
+
+      var updateMeter=function() {
+          if(meters.length>0) {
+              var meterid = meters.pop();
+              discovergy(db,meterid,function() {
+                  updateMeter();
+              });
+          } else {
+              console.log("Start seeding");
+              // process.exit();
+          }
+      }
+      updateMeter();
+      setInterval(updateMeter,process.env.IDLE_REPUBLISH);
+
     }
-    updateMeter();
-    setInterval(updateMeter,process.env.IDLE_REPUBLISH);
+    var retrieveConfig = function() {
+        db.get("config-discovergy").then(function(doc) {
+          var config = JSON.parse(decrypt(doc.config,process.env.PRIVATEKEY));
+          process.env.DISCOVERGY_METERS=config.DISCOVERGY_METERS;
+          process.env.DISCOVERGY_USERNAME=config.DISCOVERGY_USERNAME;
+          process.env.DISCOVERGY_PASSWORD=config.DISCOVERGY_PASSWORD;
+          console.log("Retrieved Config");
+          doc.lastUpdate=new Date();
+          db.put(doc);
+          
+          discovergyService();
+
+        }).catch(function() {
+          if(typeof process.env.DISCOVERGY_METERS!="undefined") {
+              var config={};
+              config.DISCOVERGY_METERS=process.env.DISCOVERGY_METERS;
+              config.DISCOVERGY_USERNAME=process.env.DISCOVERGY_USERNAME;
+              config.DISCOVERGY_PASSWORD=process.env.DISCOVERGY_PASSWORD;
+              var config_encrypted=encrypt(JSON.stringify(config),process.env.PRIVATEKEY);
+              db.put({_id:'config-discovergy',config:config_encrypted}).then(function(e) {
+                  console.log("Finished Setup",e);
+                  discovergyService();
+              });
+          } else {
+              console.log("No configuration found will try again, maybe need to be published by remote peer");
+              setTimeout(function() {
+                  retrieveConfig();
+              },60000);
+          }
+      });
+    };
+    retrieveConfig();
   });
 });
